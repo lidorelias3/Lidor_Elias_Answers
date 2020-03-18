7d329253dc1c69276106cbcfa7817dc0d049fafb
Lidor_Elias <lidorelias3@gmail.com>
2020-03-18 15:59:12 +0200

    Python - wordcount fix

--- python/Word Count/wordcount.py
+++ python/Word Count/wordcount.py
@@ -6,25 +6,25 @@ def get_words_frequency(filename):
     """
         Function will get a file and return how many times each word appear in that file.
         :param filename:
-        :return sorted dict with each word frequency:
+        :return dict with each word frequency:
     """
 
     words_count = {}
     try:
         with open(filename, 'r') as file:
             # Make a list of each word with out whitespaces and quats
-            words = [word.strip().lower().replace('\"', "").replace("\'", "") for word in
-                     re.findall(r' *[a-zA-Z]+ *', file.read())]
+            words = [word.strip().lower().replace("'", "") for word in
+                     re.findall(r'[a-zA-Z\']+', file.read())]
             # Add to dictionary
             for word in words:
                 if word in words_count:
                     words_count[word] += 1
-                else:
+                elif word != '':
                     words_count[word] = 1
     except:
         print("Error while opening: {}".format(filename))
 
-    return dict(sorted(words_count.items()))
+    return words_count
 
 
 def print_words(filename):
@@ -34,7 +34,8 @@ def print_words(filename):
     :return:
     """
     print("print_words")
-    words = get_words_frequency(filename)
+    # Sort by keys
+    words = dict(sorted(get_words_frequency(filename).items()))
     for word, frequency in words.items():
         print(word, frequency)
 
@@ -47,8 +48,9 @@ def print_top(filename):
     """
     print("print_top")
     flag = 0
-    words = get_words_frequency(filename)
-    for word, frequency in words.items():
+    # Sort by values
+    words = sorted(get_words_frequency(filename).items(), key=lambda x: x[1], reverse=True)
+    for word, frequency in words:
         print(word, frequency)
         flag += 1
         if flag == 20:
