0c0f6549d2a39a3af55ac2b08f938f2be08ecbc2
Lidor_Elias <lidorelias3@gmail.com>
2020-03-25 20:52:15 +0200

    C - basics - Word Count - fix

--- C - basics/Word Count/wordCount.c
+++ C - basics/Word Count/wordCount.c
@@ -1,53 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #pragma warning(disable:4996)
 
 #define True 1
 #define False 0
 
-void append(char*, char c);
-void check_how_may_diff_words(char*);
-int check_if_word_in_array(const char[][20], int, const char*);
+void check_how_many_diff_words(char*);
+int check_if_word_in_array(const char **, int, const char*);
 
 int main()
 {
-	char text[10000];
-	FILE *f;
-	int ch;
-	int len = 0;
-
-	//Open file
-	f = fopen("lidor.txt", "r");
-	if (f == NULL)
-	{
-		printf("could not find file\n");
-	}
-	else
-	{
-		//Get each char into a string variable
-		do {
-			ch = fgetc(f);
-			text[len] = ch;
-			len++;
-		} while (ch != EOF && ch != '\0');
-		text[len - 1] = ' ';
-		text[len] = '\0';
-		check_how_may_diff_words(text);
-	}
+	check_how_many_diff_words("aladdin.txt");
 	system("PAUSE");
+	return 0;
 }
 
-//Add char at the end of string
-void append(char* s, char c) 
-{
-	int len = strlen(s);
-	s[len] = c;
-	s[len + 1] = '\0';
-}
 
 //Check if word already in the array
-int check_if_word_in_array(const char words_array[][20], int len, const char* word)
+int check_if_word_in_array(const char **words_array, int len, const char* word)
 {
 	int i = 0;
 	for (i = 0; i < len; ++i) 
@@ -61,42 +33,71 @@ int check_if_word_in_array(const char words_array[][20], int len, const char* wo
 }
 
 
-void check_how_may_diff_words(char* text)
+void check_how_many_diff_words(char* file_name)
 {
-	char words[5000][20] = { 0 };
-	char curr_word[20] = "";
-	int i = 0;
+	FILE* f = fopen(file_name, "r");
+	int len = 0;
+	char ch;
+	int curr_word_length = 0;
 	int words_counter = 0;
-	int len = strlen(text);
+	int total_length = 0;
+
+	/*Allocating memory*/
+	char **words = (char**)malloc(sizeof(char*));
+	memset(words, 0, sizeof(char*));
+	char* curr_word = (char*)malloc(sizeof(char));
+	memset(curr_word, 0, sizeof(char));
 	
-	//For each char
-	for (i = 0; i < len; ++i)
+
+	if (f == NULL)
 	{
-		if (text[i] != '\0')
-		{
-			//If it a letter we will append to curr_word
-			if (isalpha(text[i]))
+		printf("could not find file\n");
+		return;
+	}
+	else
+	{
+		//Get each char into a string variable
+		do {
+			ch = fgetc(f);
+			len++;
+			if (ch != '\0')
 			{
-				append(curr_word, text[i]);
+				//If it a letter we will append to curr_word
+				if (isalpha(ch))
+				{
+					/*Realloc more memory for current word*/
+					curr_word = (char*)realloc(curr_word, curr_word_length * sizeof(char*) + 1);
+					curr_word[curr_word_length++] = ch;
+					curr_word[curr_word_length] = '\0';
+				}
+				//Else we finish with that word
+				else
+				{
+					//If the word not in the array add it to array and add to count
+					if (isalpha(curr_word[0]) && check_if_word_in_array(words, words_counter, curr_word) == False)
+					{
+						total_length += curr_word_length;
+						/*Allocate more memory*/
+						words = (char**)realloc(words,  total_length * sizeof(char*));
+						words[words_counter] = (char*)malloc(curr_word_length * sizeof(char) + 1);
+						strcpy_s(words[words_counter], curr_word_length + 4, curr_word);
+						words_counter++;
+					}
+					memset(curr_word, 0, curr_word_length);
+					curr_word_length = 0;
+					curr_word[0] = '\0';
+				}
 			}
-			//Else we finish with that word
 			else
 			{
-				//If the word not in the array add it to array and add to count
-				if (isalpha(curr_word[0]) && check_if_word_in_array(words, words_counter, curr_word) == False)
-				{
-					strcpy_s(words[words_counter], 20, curr_word);
-					words_counter++;
-				}
-				curr_word[0] = '\0';
+				break;
 			}
-		}
-		else
-		{
-			break;
-		}
-		
+		} while (ch != EOF && ch != '\0');
+
 	}
 
 	printf("The number of diffrent words in the string is: %d\n", words_counter);
+
+	free(words);
+	free(curr_word);
 }
