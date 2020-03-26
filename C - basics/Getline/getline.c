6b5f644666a4d48dbf1b2d9836f13c6335f5aa86
Lidor_Elias <lidorelias3@gmail.com>
2020-03-26 23:42:30 +0200

    C - basics - getLine - fix

--- C - basics/Getline/getline.c
+++ C - basics/Getline/getline.c
@@ -13,30 +13,37 @@ int main()
 	/* Check if function works */
 	char *line_buf = NULL;
 	int line_count = 0;
-	size_t line_size = SIZE;
-	FILE *fp = fopen("a.txt", "r");
+	int file_size = 0;
+
+	FILE *fp = fopen("lidor.txt", "r");
 	if (!fp)
 	{
 		return EXIT_FAILURE;
 	}
-	line_buf = (char *)malloc(line_size * sizeof(char));
-	printf("%d\n", my_getline(&line_buf, &line_size, fp));
+	/*Get file size*/
+	fseek(fp, 0L, SEEK_END);
+	file_size = ftell(fp);
+	fseek(fp, 0L, SEEK_SET);
+	
+	/*Allocate memory for that file*/
+	line_buf = (char *)malloc(file_size*sizeof(char));
+	printf("%d\n", my_getline(&line_buf, &file_size, fp));
 	printf("%s\n", line_buf);
+	
+	/*Close file and free memory*/
+	fclose(fp);
+	free (line_buf);
 	system("PAUSE");
+	return 0;
 }
 
 int my_getline(char** buffer, size_t* bufsize, FILE* stream)
 {
 	/* Function will get line from stream and return the length of the line*/
-	char line[SIZE];
-	int new_line_index;
-
-	fgets(line, SIZE, stream);
+	fgets(*buffer, bufsize, stream);
 
 	/* Find the index of new line and replace with null byte*/
-	new_line_index = (int)(strchr(line, '\n') - line);
-	line[new_line_index] = '\0';
-	/* Copy the line to the given buffer */
-	strcpy(*buffer, line);
-	return strlen(line);
+	*(buffer + (int)(strchr(*buffer, '\n') - *buffer)) = '\0';
+
+	return strlen(*buffer);
 }
