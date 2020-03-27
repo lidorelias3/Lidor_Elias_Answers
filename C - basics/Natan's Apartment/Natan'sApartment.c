ee84b7e38a87d67c0a7668c79fdcf1ec51936937
Lidor_Elias <lidorelias3@gmail.com>
2020-03-27 03:10:22 +0300

    C - basics - Natan'sApartment - fix

--- C - basics/Natan's Apartment/Natan'sApartment.c
+++ C - basics/Natan's Apartment/Natan'sApartment.c
@@ -1,8 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define SIZE 32
+#define SIZE 5
 #define FALSE 0
 #define TRUE !FALSE
 
@@ -14,10 +15,7 @@ char** all_combinations(char** arr, int length);
 int main()
 {
 	char** options = return_result("1357");
-	for (size_t i = 0; i < 150; i++)
-	{
-		printf("%s\n", options[i]);
-	}
+
 	system("PAUSE");
 }
 
@@ -29,6 +27,43 @@ char** return_result(char* str)
 	char** combination_array = all_combinations(options, strlen(str));
 	return combination_array;
 }
+
+
+char** get_options(char remembered_code[])
+{
+	/*Get a password and return the options for the password*/
+	int i = 0;
+	int j = 0;
+	char curr_option[SIZE] = "";
+
+	/*Allocate memory for string array*/
+	char **all_options = (char**)malloc(strlen(remembered_code) * sizeof(char*));
+	memset(all_options, 0, strlen(remembered_code) * sizeof(char));
+	for (i = 0; i < strlen(remembered_code); ++i)
+	{
+		all_options[i] = (char*)malloc((1 + SIZE) * sizeof(char));
+		memset(all_options[i], 0, (1 + SIZE) * sizeof(char));
+	}
+
+	/*For each number in password*/
+	for (i = 0; i < strlen(remembered_code); ++i)
+	{
+		/*Get all the option including the same number*/
+		strncat(all_options[i], &remembered_code[i], 1);
+
+		get_number_options(remembered_code[i], curr_option);
+		for (j = 0; j < strlen(curr_option); ++j)
+		{
+			if (curr_option[j] != '-')
+			{
+				strncat(all_options[i], &curr_option[j], 1);
+			}
+		}
+	}
+	return all_options;
+}
+
+
 
 char* get_number_options(char number, char* options)
 {
@@ -61,72 +96,39 @@ return options;
 }
 
 
-char** get_options(char remembered_code[])
-{
-	/*Get a password and return the options for the password*/
-	int i = 0;
-	int j = 0;
-	char curr_option[SIZE] = "";
-	/*Allocate memory for string array*/
-	char **all_options = (char**)malloc(strlen(remembered_code)*sizeof(char*));
-	for (i = 0; i < strlen(remembered_code); i++)
-	{
-		all_options[i] = (char*)malloc(SIZE * sizeof(char));
-		memset(all_options[i], 0, SIZE);
-	}
-	/*For each number in password*/
-	for (i = 0; i < strlen(remembered_code); ++i)
-	{
-		/*Get all the option including the same number*/
-		strncat(all_options[i], &remembered_code[i], 1);
-		memset(curr_option, 0, SIZE);
-		get_number_options(remembered_code[i], curr_option);
-		for(j = 0; j < strlen(curr_option); ++j)
-		{
-			if (curr_option[j] != '-')
-			{
-				strncat(all_options[i], &curr_option[j], 1);
-			}
-		}
-	}
-	return all_options;
-}
-
-
 char** all_combinations(char** arr, int length)
 {
 	int cont = TRUE;
 	int i = 0;
 	int n = 0;
 	int last = 0;
-	int* count_arr = (int*)malloc(length * sizeof(int));
+	int* index_arr = (int*)malloc(length * sizeof(int));
 
 	/*Creating the return array*/
-	char **combinations = (char**)malloc(SIZE * SIZE * length * sizeof(char*));
-	for (i = 0; i <= SIZE * SIZE * length; i++)
-	{
-		combinations[i] = (char*)malloc(length * sizeof(char));
-		memset(combinations[i], 0, length);
-	}
+	char **combinations = (char**)malloc(sizeof(char*));
 
-	/*Zero all count array*/
-	for (int i = 0; i < length; i++)
+	/*Zero all index array*/
+	for (i = 0; i < length; ++i)
 	{
-		count_arr[i] = 0;
+		index_arr[i] = 0;
 	}
 
 	while (cont)
 	{
 		/*Add the current combination to combinations array*/ 
-		for (i = 0; i < length; i++)
+		combinations = (char**)realloc(combinations, (last + 1) * sizeof(char*));
+		combinations[last] = (char*)malloc(length * sizeof(char));
+		memset(combinations[last], 0, length);
+		for (i = 0; i < length; ++i)
 		{
-			strncat(combinations[last], &arr[i][count_arr[i]], 1);
+			strncat(combinations[last], &arr[i][index_arr[i]], 1);
 		}
+		printf("%s\n", combinations[last]);
 		last++;
 
 		/*Find the next element*/
 		int n = length - 1;
-		while (n >= 0 && (count_arr[n] + 1 >= strlen(arr[n])))
+		while (n >= 0 && index_arr[n] >= strlen(arr[n]) - 1)
 		{
 			n--;
 		}
@@ -136,11 +138,11 @@ char** all_combinations(char** arr, int length)
 			cont = FALSE;
 		}
 		/*Move to next number in current array*/
-		count_arr[n]++;
+		index_arr[n]++;
 		/*Reset all rest arrays*/
-		for (i = n + 1; i < length; i++)
+		for (i = 1 + n; i < length; ++i)
 		{
-			count_arr[i] = 0;
+			index_arr[i] = 0;
 		}
 	}
 	return combinations;
