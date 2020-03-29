d230c19c21e3aa88b1a8b83dd4fc5bd89acd2bdf
Lidor_Elias <lidorelias3@gmail.com>
2020-03-29 11:00:54 +0300

    C - basics - getline - fix

--- C - basics/Getline/getline.c
+++ C - basics/Getline/getline.c
@@ -25,6 +25,7 @@ int main()
 	
 	/*Allocate memory for that file*/
 	line_buf = (char *)malloc(file_size*sizeof(char));
+	memset(line_buf, 0, file_size);
 	printf("%d\n", my_getline(&line_buf, &file_size, fp));
 	printf("%s\n", line_buf);
 	
@@ -38,7 +39,14 @@ int main()
 int my_getline(char** buffer, size_t* bufsize, FILE* stream)
 {
 	/* Function will get line from stream and return the length of the line*/
-	fgets(*buffer, bufsize, stream);
+	int i = 0;
+	char c = "";
+	
+	for (i = 0; i < bufsize && c != '\n'; ++i)
+	{
+		c = getc(stream);
+		*(*buffer + i) = c;
+	}
 
 	/* Find the index of new line and replace with null byte*/
 	*(buffer + (int)(strchr(*buffer, '\n') - *buffer)) = '\0';
