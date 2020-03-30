283013088b05aadeb659617d73b5b712ed505317
Lidor_Elias <lidorelias3@gmail.com>
2020-03-30 20:38:19 +0300

    C - Advanced - ZaziBazazi

---
+++ C - Advenced/zaziBazazi/zaziBazazi.c
@@ -0,0 +1,20 @@
+﻿#include <fcntl.h>
+#include <io.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	_setmode(_fileno(stdout), _O_U16TEXT);
+
+	CONSOLE_FONT_INFOEX info = { 0 };
+	info.cbSize = sizeof(info);
+	info.dwFontSize.Y = 20;
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), NULL, &info);
+	
+	while (1)
+	{
+		getch(stdin);
+		wprintf(L"יזאזאב יזאז\n");
+	}
+}
